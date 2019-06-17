fb175c0f4aaa62566a01defe09ca517ca347b821
Jacob Pashman <jacobpashman@gmail.com>
2019-06-16 22:35:39 -0700

    initial

--- TandemTest/ContentView.swift
+++ TandemTest/ContentView.swift
@@ -10,14 +10,71 @@ import SwiftUI
 
 struct ContentView : View {
     var body: some View {
-        Text("Hello World")
+        VStack {
+			ZStack {
+				Image("background").resizable()
+					.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: Alignment.topLeading)
+					.scaledToFill()
+				Image("arrows").resizable()
+					.frame(minWidth: 0, maxWidth: 300, minHeight: 0, maxHeight: 200, alignment: Alignment.topLeading)
+				Image("mtb sample")
+					.cornerRadius(20)
+			}
+			
+			VStack {
+				//name and location
+				VStack {
+					Text("Jacob Pashman")
+						.bold()
+					Text(" Mill Valley, CA")
+				}
+				.padding()
+				//stats
+				VStack {
+					Text("Rides 3-5 days a week")
+					Text("Rides 50-70 miles a week")
+					Text("Has been riding for 3-4 years")
+				}
+				.padding()
+				//buttons
+				HStack {
+					extractedButtons(name: "chatButton")
+					extractedButtons(name: "stravaButton")
+				}
+				.padding(.bottom)
+			}
+        }
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		Group {
+			ContentView()
+				.previewDevice(PreviewDevice(rawValue: "iPhone SE"))
+				.previewDisplayName("iPhone SE")
+			
+			ContentView()
+				.previewDevice(PreviewDevice(rawValue: "iPhone XS Max"))
+				.previewDisplayName("iPhone XS Max")
+			ContentView()
+				.previewDevice(PreviewDevice(rawValue: "iPhone XS"))
+				.previewDisplayName("iPhone XS")
+			ContentView()
+				.previewDevice(PreviewDevice(rawValue: "iPhone 8"))
+				.previewDisplayName("iPhone 8")
+		}
+	}
 }
 #endif
+
+struct extractedButtons : View {
+	var name: String
+	
+    var body: some View {
+        return Image(name).resizable()
+            .frame(minWidth: 0, maxWidth: 100, minHeight: 0, maxHeight: 66, alignment: Alignment.topLeading)
+		
+    }
+}
