2328197328195aa2d4c44f7a82f0ded3a5a4e06d
Jacob Pashman <jacobpashman@gmail.com>
2019-06-16 19:27:13 -0700

    Initial Commit

---
+++ TandemTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TandemTest
+//
+//  Created by Jacob Pashman on 6/16/19.
+//  Copyright © 2019 jacobpashman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
